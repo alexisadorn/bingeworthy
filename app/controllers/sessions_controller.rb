7ba4a34d03aaeef3a8f65ca7ef06a2c8e5bff233
alexisadorn <alexisadorn@gmail.com>
2018-05-28 16:56:39 -0500

    Logging in functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,26 @@
 class SessionsController < ApplicationController
-  def login
+  helper_method :logged_in?
+  def new
+    if logged_in?
+      redirect_to stations_path
+    end
   end
 
-  def set_session
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to stations_path
+    else
+      flash.now[:danger] = "Wrong email and/or password"
+      render :new
+    end
   end
 
-  def logout
+  def destroy
+    if logged_in?
+      session.clear
+      redirect_to '/'
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to stations_path
     else
-      redirect_to new_user_path
+      render :new
     end
   end
 
