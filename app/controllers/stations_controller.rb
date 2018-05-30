3469b4fa9afd168e636c2cda3d2609a20b16fdf3
alexisadorn <alexisadorn@gmail.com>
2018-05-30 18:39:30 -0500

    New and create station functionality

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -6,17 +6,34 @@ class StationsController < ApplicationController
     @favorites = StationShow.my_favorites(@user.id)
   end
 
-  def show
-    @station = Station.find_by(id: params[:id])
-  end
-
   def new
     @station = Station.new
+  end
+
+  def create
+    @station = Station.new(station_params)
+    if @station.save
+      redirect_to station_path(@station)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @station = Station.find_by(id: params[:id])
   end
 
   def edit
   end
 
+  def update
+  end
+
   def destroy
+  end
+
+  private
+  def station_params
+    params.require(:station).permit(:name, :description, :user_id)
   end
 end
