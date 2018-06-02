7e3c701d9eb94cd773581ca9e1f3248e1655d869
alexisadorn <alexisadorn@gmail.com>
2018-06-02 12:39:16 -0500

    Form to add show to current user's station

--- app/controllers/station_shows_controller.rb
+++ app/controllers/station_shows_controller.rb
@@ -7,7 +7,12 @@ class StationShowsController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    station_show = StationShow.new(station_show_params)
+    if station_show.save
+      redirect_to show_path(station_show.show_id)
+    else
+      redirect_to 'shows/show'
+    end
   end
 
   def edit
@@ -17,5 +22,10 @@ class StationShowsController < ApplicationController
   end
 
   def destory
+  end
+
+  private
+  def station_show_params
+    params.require(:station_show).permit(:station_id, :show_id, :user_id, :user_status, :user_season, :fav)
   end
 end
