b8336cb3dc8fe85c5ee2e72819a8d195470f3ffe
alexisadorn <alexisadorn@gmail.com>
2018-06-02 17:01:41 -0500

    Refactoring layout, partial station create forms, edit shows only for user who created the show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,11 +1,13 @@
 class ShowsController < ApplicationController
-  helper_method :current_user
+  before_action :require_login
+  before_action :set_show, only: [:show, :edit, :update]
+  before_action :created_by_current_user, only: [:edit, :update]
+
   def index
     @shows = Show.all
   end
 
   def show
-    @show = Show.find_by(id: params[:id])
     @user_shows = current_user.shows.where(id: @show.id)
   end
 
@@ -24,11 +26,9 @@ class ShowsController < ApplicationController
   end
 
   def edit
-    @show = Show.find_by(id: params[:id])
   end
 
   def update
-    @show = Show.find_by(id: params[:id])
     @show.update(show_params)
     if @show.save
       redirect_to show_path(@show)
@@ -45,9 +45,21 @@ class ShowsController < ApplicationController
                                   :day,
                                   :time,
                                   :current_season,
+                                  :created_by,
                                   genre_ids:[],
                                   genres_attributes:[:name],
                                   channel_attributes:[:name],
                                   station_shows_attributes:[:station_id, :user_status, :user_season, :fav, :user_id])
+  end
+
+  def set_show
+    @show = Show.find_by(id: params[:id])
+  end
+
+  def created_by_current_user
+    unless @show.created_by == current_user.id
+      flash[:danger] = "You cannot edit this show because you did not create it!"
+      redirect_to show_path(@show)
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_29_222814) do
+ActiveRecord::Schema.define(version: 2018_06_02_212930) do
 
   create_table "channels", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_05_29_222814) do
     t.integer "current_season"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "created_by"
   end
 
   create_table "station_shows", force: :cascade do |t|
