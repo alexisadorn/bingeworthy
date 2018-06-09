4499fd66fb86d0802d304d0b437dd6ff5d659f17
alexisadorn <alexisadorn@gmail.com>
2018-06-08 21:17:35 -0500

    Cleaning up listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,7 @@
 class ListingsController < ApplicationController
+  before_action :require_login
+  before_action :set_listing, only: [:edit, :update, :destroy]
+
   def new
     @listing = Listing.new
     @listing.show = Show.find_by(id: params[:show_id])
@@ -15,11 +18,9 @@ class ListingsController < ApplicationController
   end
 
   def edit
-    @listing = Listing.find_by(id: params[:id])
   end
 
   def update
-    @listing = Listing.find_by(id: params[:id])
     @listing.update(listing_params)
     if @listing.save
       redirect_to show_path(@listing.show)
@@ -29,7 +30,6 @@ class ListingsController < ApplicationController
   end
 
   def destroy
-    @listing = Listing.find_by(id: params[:id])
     if @listing
       @listing.destroy
       redirect_to watchlists_path
@@ -41,5 +41,9 @@ class ListingsController < ApplicationController
   private
   def listing_params
     params.require(:listing).permit(:watchlist_id, :user_id, :user_status, :user_season, :fav)
+  end
+
+  def set_listing
+    @listing = Listing.find_by(id: params[:id])
   end
 end
